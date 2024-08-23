b0801cf13b799b285710dede63138a82ad82148e
Sumit Pathak <sumit.pathak0606@gmail.com>
2024-08-23 21:40:27 +0530

    Added Chi router and logging middleware

--- main.go
+++ main.go
@@ -1,15 +1,30 @@
 package main
+
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 
 
 func main(){
+
+	router := chi.NewRouter()
+
+	//!USED TO SHOW LOGS IN TERMINAL WHILE ROUTING
+	router.Use(middleware.Logger)
+	
+	//!ADDING DEFAULT GET METHOD
+
+	router.Get("/hello",basicHandler)
+
+
 	server := &http.Server{
 		Addr: ":3000",
-		Handler: http.HandlerFunc(basicHandler),
+		Handler: router,
 	}
 
 	err:= server.ListenAndServe()
